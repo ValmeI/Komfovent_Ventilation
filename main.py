345ad92881c0518d2a66f5a5c4bb883a9e5b54d4
Ignar <Ignarvalme@gmail.com>
2022-06-05 18:23:16 +0300

    nr update

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import datetime
 today_str = datetime.datetime.now().isoformat(' ', 'seconds')
 
 '# add data from api-s to one big list. You need to know your ventilation local IP aadress'
-combined_data_list = get_vent_stats("http://192.168.1.60/", 'det') + \
-                     get_vent_stats("http://192.168.1.60/", 'i2') + \
-                     get_vent_stats("http://192.168.1.60/", 'i')
+combined_data_list = get_vent_stats("http://192.168.50.78/", 'det') + \
+                     get_vent_stats("http://192.168.50.78/", 'i2') + \
+                     get_vent_stats("http://192.168.50.78/", 'i')
 
 '# add today-s date the beginning of the list'
 new_vent_data_list = add_xpos_in_list(today_str, 0, combined_data_list)
