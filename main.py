b7fd53f02831ff1d2efbdf92deb8fcacbcc0cb94
Ignar <Ignarvalme@gmail.com>
2022-08-02 11:04:15 +0300

    printing formatting

--- main.py
+++ main.py
@@ -13,4 +13,3 @@ combined_data_list = get_vent_stats(komfovent_local_ip="http://192.168.50.78/", 
 new_vent_data_list = add_xpos_in_list(var=today_str, pos=0, input_list=combined_data_list)
 write_to_excel(excel_name="SampleData", list_of_data=new_vent_data_list)
 print(f'Success: {today_str}')
-
